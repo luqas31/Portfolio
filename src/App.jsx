6a7b70cbafcfd04d1505a0951a2913f3955754c6
luqas31 <lucas.ers@icloud.com>
2024-02-28 16:56:40 +0000

    feat: added some imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Link, NavLink, Outlet } from 'react-router-dom';
 function App() {
-	return <></>;
+	return (
+		<div className='app'>
+			<header>
+				<nav></nav>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-
-  <App />
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
