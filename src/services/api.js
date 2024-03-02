1fc0c0f08f2dcfab6d154feba1195ab32e1ed55c
luqas31 <lucas.ers@icloud.com>
2024-03-02 17:11:15 +0000

    feat: api changes

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create();
 
 export async function getData() {
-	const response = await api.get('/public/data/data.json');
+	const response = await api.get('/data/data.json');
 
 	return response.data;
 }
