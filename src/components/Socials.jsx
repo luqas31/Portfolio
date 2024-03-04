d750af7bbb21f4bb71db649b0b9cd60fb2a80c06
luqas31 <lucas.ers@icloud.com>
2024-03-04 20:17:11 +0000

    feat: updated socials

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -6,16 +6,16 @@ function Socials() {
 	return (
 		<div className='socials'>
 			<div className='socials-container'>
-				<a href='https://github.com/luqas31' className='github'>
+				<a href='https://github.com/luqas31' className='github' target='_blank'>
 					<FontAwesomeIcon icon={faGithub} size='2x' />
 				</a>
-				<a href='https://www.linkedin.com/in/luqas/' className='linkedin'>
+				<a href='https://www.linkedin.com/in/luqas/' className='linkedin' target='_blank'>
 					<FontAwesomeIcon icon={faLinkedin} size='2x' />
 				</a>
-				<a href='' className='instagram'>
+				<a href='https://www.instagram.com/lucasplss' className='instagram' target='_blank'>
 					<FontAwesomeIcon icon={faInstagram} size='2x' />
 				</a>
-				<a href='' className='threads'>
+				<a href='https://t.me/lucasplss' className='threads' target='_blank'>
 					<FontAwesomeIcon icon={faThreads} size='2x' />
 				</a>
 			</div>
